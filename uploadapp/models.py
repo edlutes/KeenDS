62e900719e1748d36b6e4dae104499dc57128153
Ed Lutes <ed_lutes@yahoo.com>
2019-06-30 20:46:02 -0400

    something about a slug being longer than 10chars

--- uploadapp/models.py
+++ uploadapp/models.py
@@ -7,22 +7,22 @@ class File(models.Model):
 
 
 class Row(models.Model):
-    Observation_Date = models.CharField(max_length=100)
-    Retail_Sales_Volume = models.FloatField(max_length=100, default = 0)
-    Retail_Price = models.FloatField(max_length=100, default = 0)
-    Acv_Distribution =models.FloatField(max_length=100, default = 0)
-    Competition = models.FloatField(max_length=100, default = 0)
-    Edlp = models.FloatField(max_length=100, default = 0)
-    Trade_Any_Promo = models.FloatField(max_length=100, default = 0)
-    Tv = models.FloatField(max_length=100, default = 0)
-    Print_Ad = models.FloatField(max_length=100, default = 0)
-    Online_Display = models.FloatField(max_length=100, default = 0)
-    Preroll_Video = models.FloatField(max_length=100, default = 0)
-    Paid_Search = models.FloatField(max_length=100, default = 0)
-    Public_Relations = models.FloatField(max_length=100, default = 0)
-    Fsi = models.FloatField(max_length=100, default = 0)
-    Digital_Coupon = models.FloatField(max_length=100, default = 0)
-    Shopper_Marketing = models.FloatField(max_length=100, default = 0)
+    Observation_Date = models.CharField(max_length=255)
+    Retail_Sales_Volume = models.FloatField(max_length=255, default = 0)
+    Retail_Price = models.FloatField(max_length=255, default = 0)
+    Acv_Distribution =models.FloatField(max_length=255, default = 0)
+    Competition = models.FloatField(max_length=255, default = 0)
+    Edlp = models.FloatField(max_length=255, default = 0)
+    Trade_Any_Promo = models.FloatField(max_length=255, default = 0)
+    Tv = models.FloatField(max_length=255, default = 0)
+    Print_Ad = models.FloatField(max_length=255, default = 0)
+    Online_Display = models.FloatField(max_length=255, default = 0)
+    Preroll_Video = models.FloatField(max_length=255, default = 0)
+    Paid_Search = models.FloatField(max_length=255, default = 0)
+    Public_Relations = models.FloatField(max_length=255, default = 0)
+    Fsi = models.FloatField(max_length=255, default = 0)
+    Digital_Coupon = models.FloatField(max_length=255, default = 0)
+    Shopper_Marketing = models.FloatField(max_length=255, default = 0)
 
     
 
