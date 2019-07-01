582d7d86965c2322a319cbdb2ff645fddf0be886
Ed Lutes <ed_lutes@yahoo.com>
2019-06-30 20:53:56 -0400

    where is this slug?

--- uploadapp/urls.py
+++ uploadapp/urls.py
@@ -6,8 +6,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('', FileUploadView.as_view()),
-    path('d/', DataList.as_view()),
-    #path('data/<int:pk>', DataDetail.as_view())
+    path('data/', DataList.as_view()),
+    path('data/<int:pk>', DataDetail.as_view())
     # url(r'^.*', TemplateView.as_view(template_name="index.html"), name="index")
 
 ]
